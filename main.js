5bcd02f1a78d24029a1bee9826ed34ec7375dd87
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-03-13 16:01:03 +1100

    implement regex

--- main.js
+++ main.js
@@ -1,24 +1,59 @@
+/* 
+"(-?[0-9]+)" + //digit
+           "( *" + // skip spaces
+              "([\+\-\*\/])" + // operator
+                           " *" + //skip spaces
+                              "(-?[0-9]+))*" //digit
+*/
+
+
 function getString(){
   inputString = document.getElementById("number1").value;
-  let stringArray = [];
-  let operator = "+-*/";
 
-  for(let i=0; i< operator.length; i++){
-    if(inputString.indexOf(operator[i]) != -1){
-      op = operator[i];
-    }
-  }
+  const firstNumRegex = /-?[0-9]+/;
 
-  stringArray = inputString.split(op);
-  console.log(stringArray);
+  const firstNum = firstNumRegex.exec(inputString)[0];
+
+  let operations = [firstNum];
   
-  console.log(stringArray[0])
-  console.log(stringArray[1])
-  console.log(op)
 
-  return stringArray[0], stringArray[1], op;
+  // group of operator & number
+  const nextGroupRegex = / *([\+\-\*\/]) *(-?[0-9]+)/g;
+
+  // skip the first number
+  nextGroupRegex.lastIndex = firstNum.length;
+  
+  let match;
+  while ((match = nextGroupRegex.exec(inputString)) != null) {
+      //push the operator
+      operations.push(match[1]);
+      //push the number
+      operations.push(match[2]);
+  }
+  console.log(operations);
 }
 
-function calculate(num1, num2, op) {
-    
+function calculate() {
+  arrayList = getString();
+  num1 = arrayList[0];
+  num2 = arrayList[1];
+  op = arrayList[2];
+  if (op == "+") {
+      return parseFloat(num1) + parseFloat(num2);
+  }
+  if (op == "-") {
+    return parseFloat(num1) - parseFloat(num2);
+  }
+  if (op == "*") {
+    return parseFloat(num1) * parseFloat(num2);
+  }
+  if (op == "/") {
+    return parseFloat(num1) / parseFloat(num2);
+  } else {
+    return alert("Something ain't right");
+  }
+}
+
+function showValue(){
+  document.getElementById("number2").value = calculate();
 }
